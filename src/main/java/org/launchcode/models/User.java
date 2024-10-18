ca16b9101d969c51a7e4f8d2ef87747952d816ea
jwaynick99 <jwaynick99@gmail.com>
2024-10-17 20:35:27 -0500

    Chapter 15 Studio - Spa Day user sign up hopefully final

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,15 +1,32 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Username required.")
+    @Size(min = 5, max = 15, message = "Username should be between 5 and 15 characters.")
     private String username;
+
+    @Email(message = "Email is invalid.")
     private String email;
+
+    @NotBlank(message = "Password is required.")
+    @Size(min = 6, message = "Password must be at LEAST 6 characters long.")
     private String password;
+
+    @NotNull(message = "Password does not match.")
+    private String verifyPassword;
+
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
@@ -38,6 +55,24 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    private void checkPassword(){
+        if (password != null || verifyPassword != null){
+            if (!password.equals(verifyPassword)){
+                verifyPassword = null;
+            }
+        }
     }
 }
 
